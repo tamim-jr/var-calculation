47f1a8c8dea068b1d2be401e21f0d4490faeae66
Tamim Mahmud <internationalinf24@gmail.com>
2022-06-08 23:34:54 +0600

    calculation in js

---
+++ index.js
@@ -0,0 +1,29 @@
+var price1 = 55;
+var price2 = 34;
+var total = price1 + price2;
+console.log(total);
+
+var price3 = 33;
+var price4 = 23;
+var total1 = price3 - price4;
+console.log(total1);
+
+var price5 = 11;
+var price6 = 22;
+price6 = price6 + 1;
+console.log(price6);
+var total2 = price5 * price6;
+console.log(total2);
+
+var price7 = 42;
+var price8 = 6;
+var total3 = price7 / price8;
+console.log(total3); 
+
+var price9 = 42;
+var price10 = 7;
+// price9++
+// price9--
+// console.log(price9);
+var total4 = price9 / price10;
+console.log(total4);
